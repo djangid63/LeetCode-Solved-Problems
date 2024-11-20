0bd0d38bb96271fa6dec0d98cd5c0a9509507855
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2024-11-20 15:49:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (28.52%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int score = 0;
+        int total = 0;
         int n = s.length();
         for(int i = 0; i < n-1; i++){
-            score += abs(s.at(i)- s.at(i + 1));
+            total += abs(s.at(i)- s.at(i + 1));
 
         }
-        return score;
+        return total;
     }
 };
