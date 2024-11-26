4f09b35ce8972fa45abadaeacdb34cd2d7e01c15
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2024-11-26 20:18:22 +0530

    Time: 3 ms (16.79%) | Memory: 22.7 MB (5.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
     
     int count = 1; 
     for (int i = 1; i < nums.size(); i++) {
-        if (nums[i] != nums[count - 1]) { 
+        if (nums[i] != nums[i - 1]) { 
             nums[count] = nums[i];
             count++;
         }
