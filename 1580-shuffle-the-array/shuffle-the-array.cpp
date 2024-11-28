6916e190ec33d0fb5cfc00b949e732a65e0b9d13
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2024-11-28 19:12:17 +0530

    Time: 5 ms (28.84%) | Memory: 13.3 MB (7.50%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
 
-        vector<int>res;
+        vector<int>res(2 * n);
 
         for(int i = 0; i < n ; i++){
-        res.push_back(nums[i]);
-        res.push_back(nums[i + n]);
+        // res.push_back(nums[i]);
+        // res.push_back(nums[i + n]);
+
+        res[2 *i] = nums[i];
+        res[2 * i + 1] = nums[n + i];
+
         }
         return res;
     }
