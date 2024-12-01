3d1ef811eaf69f7f58a19fa7c88948dca9dca203
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2024-12-01 14:17:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (5.78%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -7,9 +7,13 @@ public:
 
     while (j >= 0) {
         if (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[q--] = nums1[i--];
+            nums1[q] = nums1[i];
+            q--;
+            i--;
         } else {
-            nums1[q--] = nums2[j--];
+            nums1[q] = nums2[j];
+            q--;
+            j--;
         }
     }
     }
