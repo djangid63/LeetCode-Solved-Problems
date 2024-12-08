39d1f9e144308bad76d1ce5a3eda82d23db41516
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2024-12-08 17:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (5.63%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int size = num.length();
+            int even = 0;
+            int odd = 0;
+        for(int i = 0; i < size; i++){
+            if(i % 2 == 0){
+             even += num[i] - '0';
+            }else{
+             odd += num[i] - '0';
+            }
+        }
+
+        return odd == even;
+    }
+};
