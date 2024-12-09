14b8f7a56af57941b1d9d23c558af2e91525b539
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2024-12-09 22:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.85%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0;
+        int j = 0;
+        string result;
+        while (i < word1.size() || j < word2.size()) {
+            if (i < word1.size())
+                result += word1[i++];
+            if (j < word2.size())
+                result += word2[j++];
+        }
+        return result;
+    }
+};
